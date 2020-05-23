4a9c57db6435a1676dadf4b9058bee393e65ecdd
Andrew K. Milton <akm@unyx.net>
2020-05-23 11:56:27 -0700

    Update setup.py
    Version Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='vindauga',
-    version='0.0.12',
+    version='0.0.14',
     packages=find_packages(exclude=('test', 'build', 'dist')),
     long_description=long_description,
     long_description_content_type='text/markdown',
